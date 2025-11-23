af4b1834ca673d1479ed14b0480d1120ea3407fd
Lazyfarmerer <131164959+Lazyfarmerer@users.noreply.github.com>
2025-11-24 01:00:22 +0900

    살짝 정리

--- js/pages/sites.js
+++ js/pages/sites.js
@@ -5,8 +5,7 @@
 const sidebar_list_group = document.querySelector(".sidebar-sticky > .list-group")
 const card_list_group = document.querySelector("#card-list")
 
-// fetch_data()
-// fetch_test_data()
+
 fetch_spreadsheet_data()
 
 function fetch_spreadsheet_data() {
@@ -34,47 +33,6 @@ function fetch_spreadsheet_data() {
       card.setAttribute("data-url", url)
       card_list_group.appendChild(card)
     })
-  })
-}
-
-function fetch_test_data() {
-  response_items = [
-    {
-      name: "Google",
-      description: "구글의 공식 홈페이지입니다.",
-      url: "https://www.google.com"
-    },
-    {
-      name: "Naver",
-      description: "네이버의 공식 홈페이지입니다.",
-      url: "https://www.naver.com"
-    },
-    {
-      name: "Daum",
-      description: "다음의 공식 홈페이지입니다.",
-      url: "https://www.daum.net"
-    },
-    {
-      name: "GitHub",
-      description: "GitHub의 공식 홈페이지입니다.",
-      url: "https://github.com"
-    }
-  ]
-  response_items.forEach((item) => {
-    const {name: title, description: desc, url} = item
-
-    const sidebar_link = document.createElement("a")
-    sidebar_link.setAttribute("class", "list-group-item list-group-item-action")
-    sidebar_link.setAttribute("href", `#${title}`)
-    sidebar_link.textContent = title
-    sidebar_list_group.appendChild(sidebar_link)
-
-    const card = document.createElement("my-card")
-    card.setAttribute("id", title)
-    card.setAttribute("data-title", title)
-    card.setAttribute("data-desc", desc)
-    card.setAttribute("data-url", url)
-    card_list_group.appendChild(card)
   })
 }
 
